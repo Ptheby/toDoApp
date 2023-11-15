5818d6dec3b3ef5e6348b5d1a33630c1fba786e3
Paul Theby <paultheby@gmail.com>
2023-11-14 22:29:42 -0600

    middle of #2 of mark task done

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  taskList:any[]=[]
+  newTodoForm= this.formBuilder.group({
+    todoItem:''
+  })
+constructor(
+  private formBuilder: FormBuilder
+) {}
 
+addTask() {
+  const value= this.newTodoForm.value.todoItem
+  this.taskList.push({id: this.taskList.length, name:value})
+  this.newTodoForm.reset();
+}
+completed:boolean= false;
+
+markDone(value:any) {
+  value.completed =!value.completed
+  value.completed ===true ?
+}
 }
